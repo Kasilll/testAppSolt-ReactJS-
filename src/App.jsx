7089ad5af54d021ee6631dbccb934a1fc137ab23
Kosilov Alex 4917 <Skosilovs@mail.ru>
2021-03-16 14:35:29 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 		const scrollHeight = e.target.documentElement.scrollHeight
 		const scrollTop = e.target.documentElement.scrollTop
 		if (scrollHeight - (scrollTop + window.innerHeight) < 100 && people.length >= dataShowAnimate.end) {
-			setAnimateItem([ ...people.slice(dataShowAnimate.start, dataShowAnimate.end) ])
+			setAnimateItem([...people.slice(dataShowAnimate.start, dataShowAnimate.end) ])
 			setDataShowAnimate({
 				start: dataShowAnimate.start + 20,
 				end: dataShowAnimate.end + 20
@@ -31,12 +31,13 @@ function App() {
 
 	return (
 		<div>
-			{dataShowAnimate.start === 20 ? (
+			{dataShowAnimate.start === 20 ? ( // элементы без анимации
 				data.slice(0, 20).map((el, ind) => <Item key={`${ind}_${el}`} person={el} />)
 			) : (
 				data.slice(0, dataShowAnimate.start - 20).map((el, ind) => <Item key={`${ind}_${el}`} person={el} />)
 			)}
-			{animateItem.map((el, ind) => <Item key={`${ind}_${el}`} delay={(ind + 2) * 1} person={el} />)}
+			{animateItem[0] && animateItem.map((el, ind) => // слудующие 20 элементов с анимацией
+				<Item key={`${ind}_${el}`} delay={(ind + 2) * 1} person={el} />)} 
 
 			{/* можно и так, но это как не очень... :)
 			{/* {data.slice(0, 20).map((e) => <Item person={e} />)}
